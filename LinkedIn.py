ea0615c9fa4360a86c8f11d4e4b728c09bcfc51e
ChabaJosa <chabagjg@gmail.com>
2020-05-17 18:54:09 -0400

    Getting some html sections

--- LinkedIn.py
+++ LinkedIn.py
@@ -17,12 +17,39 @@ lines       = file.readlines()
 username    = lines[0]
 password    = lines[1]
 
-time.sleep(5)
+time.sleep(2)
 
 elementID   = browser.find_element_by_id('username')
 elementID.send_keys(username)
 
-time.sleep(5)
+time.sleep(3)
 
 elementID   = browser.find_element_by_id('password')
 elementID.send_keys(password)
+
+elementID.submit()
+
+# Visit your own profile
+time.sleep(35)
+visitingProfileID = '/in/chaba-josa/'
+fullLink = "https://www.linkedin.com" + visitingProfileID
+browser.get(fullLink)
+
+# Connect with suggestions 
+visitedProfiles = []
+profilesQueued  = []
+
+def getNewProfileIDs(soup, profilesQueued):
+    profilesID=[]
+    pav=soup.find('section',{"class": "pv-profile-section pv-browsemap-section profile-section artdeco-container-card ember-view"})
+    all_links = pav.findAll("a", {"class":"pv-browsemap-section__member-container mt4 pv-browsemap-section__member-container-line ember-view"})
+    for link in all_links:
+        userID = link.get("href")
+        if((userID not in profilesQueued) and (userID not in visitedProfiles)):
+            profilesID.append(userID)
+    return profilesID 
+
+getNewProfileIDs(BeautifulSoup(browser.page_source, 'html.parser'), profilesQueued)
+
+
+# https://www.youtube.com/watch?v=d6EQnjj-Bx0
